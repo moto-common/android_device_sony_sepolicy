b41c6d640d4cc5d9d6e60925bdc56ee565c4171d
electimon <electimon@gmail.com>
2022-04-24 09:25:17 +0000

    using qcom sepolicy

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,12 +1,4 @@
-# Sony Open Devices
-# SELinux policy variable definitions
 LOCAL_SEPOLICY := device/sony/sepolicy
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(LOCAL_SEPOLICY)/vendor
-
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    $(LOCAL_SEPOLICY)/system_ext/private
-
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(LOCAL_SEPOLICY)/system_ext/public
